e56f03f0eb8ab33b06dea6accaba7ed9f06cebc3
Dewa Ade-onojobi <adewalade@gmail.com>
2021-08-15 13:49:49 +0100

    chore: add initial layout setup

---
+++ layouts/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+      <footer className="flex-item-center" id="footer">
+        Made with {" "}<span id="heart-text"> ♥ </span>{" "} by helloDewa
+      </footer>
+    );
+}
+
+export default Footer;
---
+++ layouts/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Header = () => {
+  return (
+    <Head>
+      <title>A Simple Calculater</title>
+      <link rel="icon" href="/icons/favicon.png" />
+      <meta name="author" content="Ade-Onojobi Oluwadewalade" />
+      <meta name="description" content="A simple Calculator app Created by 'Dewalade" />
+    </Head>
+  );
+};
+
+export default Header;
